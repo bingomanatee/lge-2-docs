fdec02f9cc7cdaf43e4c7fa48b7555988704b34b
David delhart <dedelhart@voicebase.com>
2019-09-08 22:01:49 -0700

    work on docs; adding shopping cart example, some recipes

--- pages/recipes.js
+++ pages/recipes.js
@@ -41,6 +41,88 @@ function Recipes() {
             `}
           </pre>
         </code>
+
+        <h2>Transactional Locks</h2>
+        <p>
+          Transactions - an experimental feature - chokes the update stream until an action is fully complete.
+          This is useful in two ways:
+        </p>
+        <ul>
+          <li>
+            If you do a lot of prop-setting inside an action but you don't want to broadcast all the intermediary
+            stages, transactional locking ensures all the updates that come out of an action are synchronized.
+            This is especially useful for suppressing intermediate messages
+            to prevent, say, React's <code>.setState()</code> from getting overloaded.
+          </li>
+          <li>
+            If the action throws an error, a transactionally locked action will reset the state to its value
+            prior to the start of the action.
+          </li>
+        </ul>
+
+        <code>
+          <pre>
+            {`
+              const s = new Store({
+                actions: {
+                  switch: {
+                    action: ({actions, state}){
+                      const {a, b} = state;
+                      actions.setB(a);
+                      actions:setA(b);
+                    },
+                    info: {
+                      transaction: true
+                    }
+                  }
+                }
+              })
+              .addStateProp('a', 1, 'integer')
+              .addStateProp('b', 2', 'integer');
+            
+              const events = [];
+              s.subscribe((({state}) => events.push(state));
+              
+              s.actions.switch();
+              
+              console.log('events: ',events);
+               // [{a: 1, b: 2}, {a: 2, b: 1}];
+            `}
+          </pre>
+        </code>
+        <p>
+          Note in the above example, the state updates from <code>setB()</code> and <code>setA()</code>
+          do not appear in the event stream.
+        </p>
+        <p>
+          One minor point: transactional locking does <i>not</i> freeze <code>myStore.state</code>; it will
+          get updated in real time by any actions or property setters. It's only effect (unless an error is thrown)
+          is on the stream and subscription updating methods.
+        </p>
+        <p>Fair warning - the reverted snapshot is a deconstructed copy of the state object.
+           If you have arrays and objects that you modify instead of modifying with <code>.pop()</code> or whatever,
+           set info to <code>{`{transaction: true, clone: true}`}</code>. This will snapshot the state with lodash's
+          <code>.cloneDeep()</code> method. this, in turn, can create issues if your store properties are class
+           instances:
+          <code>.cloneDeep()</code> will break any connections between instances and their prototypes.
+        </p>
+        <h3>Nested Transactions</h3>
+        <p>Diagnosing sub-failed transactions is complex; here are two suggestions when a transactionally locked
+           action triggers another transactionally locked action: </p>
+        <ol>
+          <li>Note the last element in the transactions array at the start of the action; that is the transaction id
+              of the current action.
+          </li>
+          <li>Inside the outer action, subscribe and look for errors thrown by any sub-action calls.</li>
+          <li>If your stream detects any sub-transaction throws, at the time of the thrown error, the transactional
+              stack will include the actionId of the subTransaction (available from the subscribe watcher) and the id
+              of the outer transaction.
+          </li>
+          <li>The most manageable solution to responding to a sub-transactional failure is to throw an error
+              and revert the outer transaction. If you feel moved to do more specialized corrective surgery,
+              you can do so by watching the state update that the inner transactional reset triggers.
+          </li>
+        </ol>
       </article>
     </main>
   </div>
